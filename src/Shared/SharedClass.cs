007841df107ca5d1f4f20f4a3cd3bc1b82af4b4f
ddukbaek2 <ddukbaek2@gmail.com>
2025-10-05 15:12:17 +0900

    공유 클래스 관련 수정 + 라이선스 관련 설정 수정.

--- src/Shared/SharedClass.cs
+++ src/Shared/SharedClass.cs
@@ -1,9 +1,12 @@
+using Object = Crockhead.Core.Disposable;
+
+
 namespace Crockhead.Core
 {
 	/// <summary>
 	/// 공유 클래스.
 	/// </summary>
-	public abstract class SharedClass<TClass> : Disposable where TClass : SharedClass<TClass>, new()
+	public class SharedClass<TClass> : Object where TClass : SharedClass<TClass>, new()
 	{
 		/// <summary>
 		/// 생성 되었는지 여부 프로퍼티.
@@ -16,16 +19,36 @@ namespace Crockhead.Core
 		public static TClass Instance => Create();
 
 		/// <summary>
+		/// 현재 인스턴스가 명시적인 생성인지 여부. (직접 Create() 호출)
+		/// </summary>
+		public bool IsExplicitCreated { private set; get; }
+
+		/// <summary>
+		/// 생성시 입력한 아규먼트.
+		/// </summary>
+		public object[] Arguments { private set; get; }
+
+		/// <summary>
 		/// 생성됨.
 		/// </summary>
 		public SharedClass() : base()
 		{
-			// 중복생성의 경우 제외.
+			IsExplicitCreated = false;
+			Arguments = new object[0];
+
+			// 이미 공유 인스턴스가 존재 할 경우 제외.
 			if (SharedInstances.IsSet<TClass>())
-			{
-				//Disposables.Dispose(this);
 				return;
-			}
+
+			// 등록.
+			SharedInstances.Set<TClass>((TClass)this);
+		}
+
+		/// <summary>
+		/// 생성됨.
+		/// </summary>
+		protected virtual void OnCreate(params object[] arguments)
+		{
 		}
 
 		/// <summary>
@@ -33,36 +56,56 @@ namespace Crockhead.Core
 		/// </summary>
 		protected override void OnDispose(bool explicitDisposing)
 		{
-			if (explicitDisposing)
-			{
-				if (!SharedInstances.IsSet<TClass>())
-					return;
+			/// 현재 인스턴스가 공유 인스턴스가 아니면 제외.
+			if (!SharedClass<TClass>.IsSharedInstance((TClass)this))
+				return;
 
-				var sharedInstance = SharedInstances.Get<TClass>();
-				if (sharedInstance != this)
-					return;
-
-				SharedInstances.Unset<TClass>();
-			}
+			// 등록 해제.
+			SharedInstances.Unset<TClass>();
 		}
-
-		/// <summary>
-		/// 생성됨.
-		/// </summary>
-		protected abstract void OnCreate(params object[] arguments);
 
 		/// <summary>
 		/// 생성.
 		/// </summary>
 		public static TClass Create(params object[] arguments)
 		{
-			if (SharedInstances.TryGet<TClass>(out var sharedInstance))
-				return sharedInstance;
+			if (SharedInstances.TryGet<TClass>(out var obj))
+				return obj;
 
-			sharedInstance = new TClass();
-			SharedInstances.Set<TClass>(sharedInstance);
-			sharedInstance.OnCreate(arguments);
-			return sharedInstance;
+			obj = new TClass();
+			obj.IsExplicitCreated = true;
+			obj.Arguments = arguments;
+			obj.OnCreate(arguments);
+			return obj;
+		}
+
+		/// <summary>
+		/// 대상 인스턴스가 공유 인스턴스인지 여부.
+		/// </summary>
+		public static bool IsSharedInstance(TClass obj)
+		{
+			// 공유 인스턴스의 존재 여부.
+			if (!SharedInstances.IsSet<TClass>())
+				return false;
+
+			// 공유 인스턴스와 동일 인스턴스 여부.
+			var instance = SharedInstances.Get<TClass>();
+			if (instance != obj)
+				return false;
+
+			return true;
+		}
+
+		/// <summary>
+		/// 해제.
+		/// </summary>
+		public static bool Dispose(TClass obj)
+		{
+			if (!SharedClass<TClass>.IsSharedInstance(obj))
+				return false;
+
+			Disposables.Dispose(obj);
+			return true;
 		}
 	}
 }
