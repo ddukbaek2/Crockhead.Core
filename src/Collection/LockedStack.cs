36afd242ddddaa44d55287ee23c7c83f210899db
ddukbaek2 <ddukbaek2@gmail.com>
2025-10-24 12:20:22 +0900

    작업중.

---
+++ src/Collection/LockedStack.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Crockhead.Core
+{
+	/// <summary>
+	/// 쓰레드 안전한 스택.
+	/// </summary>
+	public class LockedQueue<T>
+	{
+	}
+}
