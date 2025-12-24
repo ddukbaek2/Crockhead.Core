8f37c3f5d23c769967ab4730af6f7d1a6661477d
ddukbaek2 <ddukbaek2@gmail.com>
2025-12-25 04:39:15 +0900

    작업.

--- src/Thread/BackStitcher.cs
+++ src/Thread/BackStitcher.cs
@@ -8,7 +8,20 @@ namespace Crockhead.Core
 	/// <summary>
 	/// 되돌아홈질.
 	/// </summary>
-	public class BackStitcher
+	public class BackStitcher : Disposable
 	{
+		/// <summary>
+		/// 생성됨.
+		/// </summary>
+		public BackStitcher() : base()
+		{
+		}
+
+		/// <summary>
+		/// 해제됨.
+		/// </summary>
+		protected override void OnDispose(bool explicitDisposing)
+		{
+		}
 	}
 }
